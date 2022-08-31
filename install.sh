cd40dfc53e6f969c3a4a3a6efdcba7f7a063a30a
akrish10 <akrish10@visteon.com>
2022-08-31 13:16:13 +0530

    Install starship along with others

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
+if ! command -v starship &> /dev/null
+then
+  curl -ksS https://starship.rs/install.sh | sh
+fi
+
+if [[ ! -d ~/.config ]]; then
+  mkdir ~/.config
+fi
+
+starship preset pastel-powerline > ~/.config/starship.toml
+
 if [ -f "${HOME}.bashrc" ]; then
 	cp -f "${HOME}.bashrc" "${HOME}.bashrc.bak"
 fi
 cp -RT dotfiles/ ~/
-
