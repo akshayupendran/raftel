75cfbad76fec9e472057c67b209d3ff9211613c7
akrish10 <akrish10@visteon.com>
2023-04-07 18:05:33 +0530

    Install some more utilities for c programming.

--- install.sh
+++ install.sh
@@ -17,26 +17,69 @@
 ### All Rights Reserved.
 ###
 
-set -ex
-if command sudo -v
-then
-  sudo apt -y update
-  sudo apt -y upgrade
-  sudo apt -y install vim
-  sudo apt -y install curl
-  sudo apt -y install python3
+set -e
+
+if command sudo -v; then
+  sudo apt -qq update
+  sudo apt -qq upgrade
+	
+	if ! command -v vim &> /dev/null
+	then
+		sudo apt -qq install vim
+	fi
+
+	if ! command -v curl &> /dev/null
+	then
+	  sudo apt -qq install curl
+	fi
+  	
+	if ! command -v python3 &> /dev/null
+	then
+		sudo apt -qq install python3
+	fi
+
+	if ! command -v hexdump &> /dev/null
+	then
+		sudo apt -qq install bsdmainutils
+	fi
+
+  if ! command -v lsb_release &> /dev/null
+	then
+		sudo apt -qq install lsb-compat
+	fi
+
+	if ! command -v podman &> /dev/null
+	then
+		echo "!!!!!!Please manually install podman!!!!!"
+	fi
+
+	if ! command -v clang-format &> /dev/null
+	then
+		sudo apt -qq install clang-format
+	fi
+
+	if ! command -v clang &> /dev/null
+	then
+		sudo apt -qq install clang
+	fi
+  
+  if ! command -v clang-tidy &> /dev/null
+	then
+		sudo apt -qq install clang-tidy
+	fi
+  
   sudo -k
 fi
 
 if [[ ! -d ~/.local ]]; then
   mkdir ~/.local
 fi
+
 if [[ ! -d ~/.local/bin ]]; then
   mkdir ~/.local/bin
 fi
 
-if ! command -v starship &> /dev/null
-then
+if ! command -v starship &> /dev/null; then
   curl -ksS https://starship.rs/install.sh -o starship_install.sh
   chmod +x starship_install.sh
   ./starship_install.sh -b ~/.local/bin
@@ -47,19 +90,23 @@ if [[ ! -d ~/.config ]]; then
   mkdir ~/.config
 fi
 
-if [[ -f ~/.bashrc ]]; then
+if [[ ! -f ~/.path ]]; then
 	mv -f ~/.bashrc ~/.bashrc.bak
+	cp -RT dotfiles/ ~/
 fi
-cp -RT dotfiles/ ~/
 
-## Load our default configuration
-~/.local/bin/starship preset pastel-powerline > ~/.config/starship.toml
+if [[ ! -f ~/.config/starship.toml ]]; then
+	~/.local/bin/starship preset pastel-powerline > ~/.config/starship.toml
+fi
+
 if ! command -v fzf &> /dev/null; then
-rm -rf ~/.fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+  rm -rf ~/.fzf
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
 fi
 
-curl -ksS https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo -o ~/.local/bin/git-filter-repo
+if [[ ! -f ~/.local/bin/git-filter-repo ]]; then
+  curl -ksS https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo -o ~/.local/bin/git-filter-repo
+fi
 
 echo "Please run source ~/.bashrc a few times to complete installation !"
