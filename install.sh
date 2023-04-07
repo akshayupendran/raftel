a52ab95e7e4320dded232301ea7d643be72e23fd
akrish10 <akrish10@visteon.com>
2023-04-07 16:22:41 +0530

    Make install.sh install some more utilities

--- install.sh
+++ install.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+
+### install.sh:
+###     git
+###     vim
+###
+### Usage:
+###     ./security_development_keys_generator.sh security_development_keys.conf
+###
+### Prerequisite:
+###     Program specific key configuration file security_development_keys.conf must be updated.
+###
+### Author:
+###     akrish10(Akshay Krishna Upendran)
+###
+### [2021] Visteon Corporation
+### All Rights Reserved.
+###
+
 set -e
-set -x
+if command sudo -v
+then
+  sudo apt -y update
+  sudo apt -y upgrade
+  sudo apt -y install vim
+  sudo apt -y install curl
+  sudo -k
+fi
 
 if [[ ! -d ~/.local ]]; then
   mkdir ~/.local
@@ -13,7 +38,7 @@ if ! command -v starship &> /dev/null
 then
   curl -ksS https://starship.rs/install.sh -o starship_install.sh
   chmod +x starship_install.sh
-  starship_install.sh -b ~/.local/bin
+  ./starship_install.sh -b ~/.local/bin
   rm -rf starship_install.sh
 fi
 
@@ -21,9 +46,16 @@ if [[ ! -d ~/.config ]]; then
   mkdir ~/.config
 fi
 
-starship preset pastel-powerline > ~/.config/starship.toml
-
 if [[ -f ~/.bashrc ]]; then
 	mv -f ~/.bashrc ~/.bashrc.bak
 fi
 cp -RT dotfiles/ ~/
+
+## Load our default configuration
+~/.local/bin/starship preset pastel-powerline > ~/.config/starship.toml
+if ! command -v fzf &> /dev/null; then
+rm -rf ~/.fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+source ~/.fzf.bash
+fi
