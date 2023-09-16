5fddad1b85e5f64c04f2102fc10b3cef387db21e
akrish10 <akrish10@visteon.com>
2023-09-17 00:17:24 +0530

    Adding stow to the mix

--- install.sh
+++ install.sh
@@ -1,121 +1,98 @@
 #!/bin/bash
 
 ### install.sh:
-###     git
-###     vim
 ###
 ### Usage:
-###     ./security_development_keys_generator.sh security_development_keys.conf
+###     ./install.sh
 ###
 ### Prerequisite:
-###     Program specific key configuration file security_development_keys.conf must be updated.
+###     Installation configuration file install.conf must be updated.
 ###
 ### Author:
 ###     akrish10(Akshay Krishna Upendran)
 ###
-### [2021] Visteon Corporation
-### All Rights Reserved.
-###
 
 set -ex
-
-if [[ ! -f ~/.path ]]; then
-  mv -f ~/.bashrc ~/.bashrc.bak
-  cp -RT dotfiles/ ~/
-fi
-
-for folder in ~/.{local,local/bin,config}; do
-  if [[ ! -d $folder ]]; then
-    mkdir $folder
-  fi
-done;
-unset folder;
-
-# Requires sudo
+# APT Installations
+declare -A g_apt_programs_dict
+source install.conf
+## Requires sudo
 if command sudo -v; then
   sudo apt -yqq update
   sudo apt -yqq upgrade
+  ### Useful scripts to add and remove PPA
   if ! command -v add-apt-repository &> /dev/null; then
-    sudo apt -yqq install software-properties-common
+    sudo apt -yqq inst:all software-properties-common
   fi
-
-# ToDo: Make sure libssl-dev is installed only once.
-  for program in vim autoconf curl python3 clang-format clang-tidy clang gcc\
-                 gdb openssl wget make man libssl-dev g++ git-lfs shellcheck\
-                 git-crypt tldr; do
-    if ! command -v $program &> /dev/null; then
-      sudo apt -yqq install $program
+  ### Install list of apt programs
+  for l_program in $g_apt_programs; do
+    if ! command -v $l_program &> /dev/null; then
+      sudo apt -yqq install $l_program
     fi
   done;
-  unset program;
-
-  if ! command -v srec_cat &> /dev/null; then
-    sudo apt -yqq install srecord
-  fi
-
-  if ! command -v named &> /dev/null; then
-    sudo apt -yqq install bind9
-  fi
-
-  if ! command -v hexdump &> /dev/null; then
-    sudo apt -yqq install bsdmainutils
-  fi
-
-  if ! command -v lsb_release &> /dev/null; then
-    sudo apt -yqq install lsb-compat
-  fi
-
-  # Requires sudo and curlrc
-  if ! command -v git-lfs &> /dev/null; then
-    curl -ksS https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh -o script.deb.sh
-    chmod +x script.deb.sh
-    sudo ./script.deb.sh
-    rm -f script.deb.sh
-    sudo apt install git-lfs
-  fi
-  sudo -k
+  unset l_program;
+  ### Install second list of apt programs
+  for l_program in "${!g_apt_programs_dict[@]}"; do
+    if ! command -v ${g_apt_programs_dict[$l_program]} &> /dev/null; then
+      sudo apt -yqq install $l_program
+    fi
+  done;
+  unset l_program;
+  sudo -k  
 fi
 
-# does not require sudo
-if ! command -v git-crypt &> /dev/null; then
-  git clone https://github.com/AGWA/git-crypt
-  cd git-crypt
-  make -j8
-  make install PREFIX=~/.local
-  cd ..
-  rm -rf git-crypt
+# Dotfiles stowing - requires stow
+if command -v stow &> /dev/null; then
+  ## Dirty check via .path if path already exists. If exists dont install.
+  if [[ ! -f ~/.path ]]; then
+    mv -f ~/.bashrc ~/.bashrc.bak
+    for l_folder in */ ; do\
+      stow --dotfiles "$l_folder"
+    done
+    unset l_folder;
+  fi
 fi
 
-# Requires curlrc
-if ! command -v starship &> /dev/null; then
-  curl -ksS https://starship.rs/install.sh -o starship_install.sh
-  chmod +x starship_install.sh
-  ./starship_install.sh -y -b ~/.local/bin
-  rm -f starship_install.sh
+for l_folder in ~/.{local,local/bin,config}; do
+  if [[ ! -d $l_folder ]]; then
+    mkdir $l_folder
+  fi
+done;
+unset l_folder;
+
+# Installation of starship -> Requires curl
+if command -v curl &> /dev/null; then
+  if ! command -v starship &> /dev/null; then
+    curl -ksS https://starship.rs/install.sh -o starship_install.sh
+    chmod +x starship_install.sh
+    ./starship_install.sh -y -b ~/.local/bin
+    rm -f starship_install.sh
+  fi 
 fi
 
-#Requires Starship
+# If default starship config does not exist, then take pastel-powerline as default.
 if [[ ! -f ~/.config/starship.toml ]]; then
   ~/.local/bin/starship preset pastel-powerline > ~/.config/starship.toml
 fi
 
+# Install FzF - better than apt version
 if ! command -v fzf &> /dev/null; then
   rm -rf ~/.fzf
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   ~/.fzf/install --all
 fi
 
+# Install Filter Repo
 if [[ ! -f ~/.local/bin/git-filter-repo ]]; then
   curl -ksS https://raw.githubusercontent.com/newren/git-filter-repo/main/git-filter-repo -o ~/.local/bin/git-filter-repo
 fi
 
-<<not_working
+# Install rustc
 if ! command -v rustc &> /dev/null; then
   curl --proto '=https' --tlsv1.2 -ksSf https://sh.rustup.rs -o temp.sh
   chmod +x temp.sh
   ./temp.sh -y
   rm -f temp.sh
 fi
-not_working
 
 echo "Please run source ~/.bashrc a few times to complete installation !"
